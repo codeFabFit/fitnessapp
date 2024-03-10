1c9bcfb674243e10c4253e09253493144d01fb0a
codeFabFit <fabgar14@gmail.com>
2024-03-10 10:43:35 -0400

    first commit set up

--- App.js
+++ App.js
@@ -4,7 +4,10 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>welcome to the FabFitForever App</Text>
+      <Text> Workout Plans at Your Pace </Text>
+      <Text> Get Fab, Feel Fit & Let It Last Forever </Text>
+
       <StatusBar style="auto" />
     </View>
   );
